3ca65f648153310ed3aed6db687972c509e6f2d4
Jordan Wamser <jwamser@redpandacoding.com>
2024-03-23 16:49:40 -0500

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -2,11 +2,6 @@
 
 echo "Setting up your Mac..."
 
-# # Check for Oh My Zsh and install if we don't have it
-# if test ! $(which omz); then
-#   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
-# fi
-
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -19,3 +14,10 @@ if test ! $(which brew); then
   # (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> $HOME/.zprofile
   # eval "$(/usr/local/bin/brew shellenv)"
 fi
+
+# Create a projects directories
+mkdir $HOME/Projects
+
+# Create Code subdirectories
+mkdir $HOME/Projects/jwamser
+mkdir $HOME/Projects/RedPandaCoding
