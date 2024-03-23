6c05b919189aa3e206a04809c0284db60761bdd3
Jordan Wamser <jwamser@redpandacoding.com>
2024-03-23 16:38:29 -0500

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -11,6 +11,11 @@ echo "Setting up your Mac..."
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+  ## M# Chip Setup
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
+
+  ## Intel Setup
+  # (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> $HOME/.zprofile
+  # eval "$(/usr/local/bin/brew shellenv)"
 fi
